5faac9f083216263812605418757bcfc9d063d51
Riki420 <girottiriccardo1998@gmail.com>
2021-09-02 16:32:20 +0200

    aggiunta la possibilità di filtrare i generi

--- js/script.js
+++ js/script.js
@@ -5,15 +5,26 @@ Vue.config.devtools = true;
 const app = new Vue({
   el: '#app',
   data: {
-    albums: []
+    albums: [],
+    selectedValue: '',
+    genres: []
   },
   methods: {
-
   },
+  computed: {
+    
+  },
+
   created(){
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((res)=> {
-      this.albums = res.data.response;
-    })
 
+      this.albums = res.data.response;
+
+      this.albums.forEach((element) => {
+          if(!this.genres.includes(element.genre)) {
+            this.genres.push(element.genre)
+          }
+      })
+    });
   }
 });
